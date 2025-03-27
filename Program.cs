9b5c1e2b17515248792a58c81c717571a7a56035
nycollassobolevski <sobolevski.academy@gmail.com>
2025-03-27 16:00:38 -0300

    feat encrypt ready

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+string key = "iTnCPKiVmpBagmQu";
+var crypto = new Crypto(key);
+
+string inPath = args[0];
+string outPath = args[1];
+
+crypto.Encrypt(inPath, outPath);
+
+// crypto.Encrypt(path, "./encrypted_dbskillhub_backup-20-03-2025_15-28");
+// crypto.Decrypt("./encrypted_dbskillhub_backup-20-03-2025_15-28", "./desencrypted_dbskillhub_backup-20-03-2025_15-28.bak");
+
+
---
+++ crypto.cs
@@ -0,0 +1,68 @@
+using System.Security.Cryptography;
+using System.Text;
+
+public class Crypto (string key)
+{
+    private string passKey = key;
+
+    public void Encrypt (string inputFile, string outputFile)
+    {
+        byte[] key = Encoding.UTF8.GetBytes(this.passKey);
+
+        using Aes aes = Aes.Create();
+
+        aes.Key = key;
+        aes.GenerateIV();
+
+        ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+        CheckIfDirExists(outputFile);
+
+        using FileStream fsInput = new (inputFile, FileMode.Open, FileAccess.Read);
+        using FileStream fsOutput = new (outputFile, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+
+        fsOutput.Write(aes.IV, 0, aes.IV.Length);
+
+        using CryptoStream csEncrypt = new (fsOutput, encryptor, CryptoStreamMode.Write);
+
+        fsInput.CopyTo(csEncrypt);
+    }
+    public void Decrypt (string inputFile, string outputFile)
+    {
+        byte[] key = Encoding.UTF8.GetBytes(this.passKey);
+
+        using Aes aes = Aes.Create();
+
+        aes.Key = key;
+
+        using FileStream fsInput = new(inputFile, FileMode.Open, FileAccess.Read);
+
+        byte[] iv = new byte[aes.IV.Length];
+        fsInput.Read(iv, 0, iv.Length);
+        aes.IV = iv;
+
+        ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+        using FileStream fsOutput = new(outputFile, FileMode.Create, FileAccess.Write);
+        using CryptoStream csDecrypt = new(fsInput, decryptor, CryptoStreamMode.Read);
+        csDecrypt.CopyTo(fsOutput);
+    }
+
+    /// <summary>
+    /// Check if directory exists, if not, create.
+    /// </summary>
+    /// <param name="filePath">This param must have filename</param>
+    private void CheckIfDirExists(string filePath)
+    {
+        string res = "";
+        string[] dirs = filePath.Split("/");
+        if(dirs.Length <= 1)
+            dirs = filePath.Split("\\");
+
+        for(int i = 0; i < dirs.Length - 1; i++)
+            res += $@"{dirs[i]}\";
+
+        if(!Directory.Exists(res))
+            Directory.CreateDirectory(res);
+    }
+}
